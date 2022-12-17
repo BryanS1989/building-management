16401369a3d74cf7d284756cb55302e9934b99c5
Bryan <bryanstevenal@gmail.com>
2022-12-17 14:48:49 +0100

    Create Roomlist component

---
+++ src/components/room/RoomList.vue
@@ -0,0 +1,101 @@
+<script>
+export default {
+    name: 'RoomList',
+    props: {
+        selectedFloor: Number,
+        rooms: Array,
+    },
+    data() {
+        return {};
+    },
+    methods: {
+        addRoom() {
+            console.log('[RoomList] [addRoom()]');
+        },
+        modifyRoom() {
+            console.log('[RoomList] [modifyRoom()]');
+        },
+        deleteRoom(index) {
+            console.log('[RoomList] [deleteRoom()] index: ' + index);
+        },
+    },
+};
+</script>
+
+<template>
+    <header class="rooms__header">
+        <h2>{{ $t('floor') }} {{ selectedFloor }}</h2>
+        <button
+            class="form__button form__button--primary"
+            @click="addRoom()"
+        >
+            <font-awesome-icon icon="fa-regular fa-square-plus" />
+            {{ $t('actions.add') }}
+        </button>
+    </header>
+
+    <article class="rooms">
+        <section
+            class="room"
+            v-for="(room, index) in rooms"
+            :key="room.number"
+            v-bind:value="room"
+        >
+            <header class="rooms__header room__title">
+                <h3 class="room__text--bold">
+                    {{ $t('room') }} {{ room.number }}
+                </h3>
+
+                <font-awesome-icon
+                    icon="fa-regular fa-trash-can"
+                    class="pointer"
+                    @click="deleteRoom(index)"
+                />
+            </header>
+
+            <form class="room__body">
+                <fieldset>
+                    <label
+                        for="maxCapacity"
+                        class="room__text--bold form__label--block"
+                    >
+                        {{ $t('maxCapacity') }}
+                    </label>
+                    <input
+                        type="number"
+                        id="maxCapacity"
+                        class="form__input"
+                        :value="room.capacity"
+                    />
+                </fieldset>
+
+                <fieldset>
+                    <label
+                        for="occupation"
+                        class="room__text--bold form__label--block"
+                    >
+                        {{ $t('occupation') }}
+                    </label>
+                    <input
+                        type="number"
+                        id="occupation"
+                        class="form__input"
+                        :value="room.occupation"
+                    />
+                </fieldset>
+            </form>
+
+            <footer class="room__actions">
+                <button
+                    class="form__button form__button--primary"
+                    @click="modifyRoom()"
+                >
+                    <font-awesome-icon icon="fa-regular fa-pen-to-square" />
+                    {{ $t('actions.modify') }}
+                </button>
+            </footer>
+        </section>
+    </article>
+</template>
+
+<style scoped></style>

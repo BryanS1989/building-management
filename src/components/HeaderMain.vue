cab268d379c10282b00636e6fad89f690332746e
Bryan <bryanstevenal@gmail.com>
2022-12-17 14:49:21 +0100

    Create main header component

---
+++ src/components/HeaderMain.vue
@@ -0,0 +1,40 @@
+<script>
+export default {
+    name: 'HeaderMain',
+    props: {
+        floors: Array,
+    },
+    emits: ['selectedFloor'],
+    data() {
+        return {
+            selectedFloor: 1,
+        };
+    },
+};
+</script>
+
+<template>
+    <header>
+        <h1>{{ $t('rooms') }}</h1>
+
+        <nav>
+            <select
+                v-model="selectedFloor"
+                @change="$emit('selectedFloor', selectedFloor)"
+                class="form__input width--auto"
+            >
+                <option
+                    v-for="floor in floors"
+                    :key="floor"
+                    v-bind:value="floor"
+                >
+                    {{ $t('floor') }} {{ floor }}
+                </option>
+            </select>
+        </nav>
+    </header>
+
+    <hr />
+</template>
+
+<style scoped></style>

1a33581cb5c1bb82342a714a61fd4c549e502e24
Bryan <bryanstevenal@gmail.com>
2022-12-17 20:29:36 +0100

    Empty App vue, only with router-view

--- src/App.vue
+++ src/App.vue
@@ -1,67 +1,14 @@
 <script>
-import HeaderMain from './components/HeaderMain.vue';
-import RoomList from './components/room/RoomList.vue';
-
 export default {
-    components: {
-        HeaderMain,
-        RoomList,
-    },
+    components: {},
     data() {
-        return {
-            selectedFloor: 1,
-            floors: [1, 2, 3],
-            rooms: [
-                {
-                    number: 1,
-                    capacity: 30,
-                    occupation: 40,
-                },
-                {
-                    number: 2,
-                    capacity: 30,
-                    occupation: 40,
-                },
-                {
-                    number: 3,
-                    capacity: 30,
-                    occupation: 40,
-                },
-                {
-                    number: 4,
-                    capacity: 30,
-                    occupation: 40,
-                },
-                {
-                    number: 5,
-                    capacity: 30,
-                    occupation: 40,
-                },
-            ],
-        };
+        return {};
     },
-    methods: {
-        printFloor(selectedFloor) {
-            console.log('[App] [printFloor()] selectedFloor: ' + selectedFloor);
-            this.selectedFloor = selectedFloor;
-        },
-    },
+    methods: {},
 };
 </script>
 
 <template>
-    <HeaderMain
-        :floors="floors"
-        @selected="printFloor"
-    />
-
-    <main>
-        <RoomList
-            :selected-floor="selectedFloor"
-            :rooms="rooms"
-        ></RoomList>
-    </main>
-
     <router-view></router-view>
 </template>
 

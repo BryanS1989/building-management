f5a6a143d9da8021bb7b2f088d8686eecd8a6208
Bryan <bryanstevenal@gmail.com>
2022-12-17 14:47:08 +0100

    Register FontAwesomeIcon global component and used icons

--- src/main.js
+++ src/main.js
@@ -6,6 +6,16 @@ import { createI18n } from 'vue-i18n';
 import './assets/main.css';
 import localeES from './locale/es.json';
 
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import {
+    faTrashCan,
+    faPenToSquare,
+    faPlusSquare,
+} from '@fortawesome/free-regular-svg-icons';
+
+library.add(faTrashCan, faPenToSquare, faPlusSquare);
+
 const i18n = createI18n({
     locale: 'es',
     messages: {
@@ -17,5 +27,7 @@ const app = createApp(App);
 
 app.use(router);
 app.use(i18n);
+
+app.component('font-awesome-icon', FontAwesomeIcon);
 
 app.mount('#app');

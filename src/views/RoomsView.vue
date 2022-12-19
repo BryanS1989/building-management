0afec22284d33fc12f9551a0bcae48a5b1306d0a
Bryan <bryanstevenal@gmail.com>
2022-12-19 18:39:45 +0100

    Remove unnecesary info

--- src/views/RoomsView.vue
+++ src/views/RoomsView.vue
@@ -6,36 +6,7 @@ export default {
         RoomList,
     },
     data() {
-        return {
-            selectedFloor: 1,
-            rooms: [
-                {
-                    number: 1,
-                    capacity: 30,
-                    occupation: 40,
-                },
-                {
-                    number: 2,
-                    capacity: 30,
-                    occupation: 40,
-                },
-                {
-                    number: 3,
-                    capacity: 30,
-                    occupation: 40,
-                },
-                {
-                    number: 4,
-                    capacity: 30,
-                    occupation: 40,
-                },
-                {
-                    number: 5,
-                    capacity: 30,
-                    occupation: 40,
-                },
-            ],
-        };
+        return {};
     },
     methods: {},
 };
